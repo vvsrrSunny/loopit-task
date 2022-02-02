9c8eee939d09aa5538b97e8ebf9751ccc0569b55
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-02-02 20:00:17 +0800

    setting up the API

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -3,18 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\Car;
-use App\Http\Requests\StoreCarRequest;
-use App\Http\Requests\UpdateCarRequest;
+use Illuminate\Http\Request;
 
 class CarController extends Controller
 {
     /**
      * Display a listing of the resource.
+     * @param Illuminate\Http\Request $request
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\Response|\Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
      */
-    public function __invoke()
+    public function __invoke(Request $request)
     {
+        if ($request->expectsJson()) {
+
+        }
+
         return view('dashboard');
     }
 }
