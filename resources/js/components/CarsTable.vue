94a670e96df308db2a4b4eded02ac97420637197
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-02-02 22:08:22 +0800

    api call function in progress

--- resources/js/components/CarsTable.vue
+++ resources/js/components/CarsTable.vue
@@ -3,10 +3,22 @@
     <template #heading>
       <table-header-cell> Car Name </table-header-cell>
       <table-header-cell> Auto make </table-header-cell>
+      <table-header-cell> Number of cars available </table-header-cell>
+      <table-header-cell> cars in stock </table-header-cell>
     </template>
     <template #body>
-      <table-cell> X 1 </table-cell>
-      <table-cell> BMW </table-cell>
+      <tr>
+        <table-cell> X 1 </table-cell>
+        <table-cell> BMW </table-cell>
+        <table-cell> BMW </table-cell>
+        <table-cell> BMW </table-cell>
+      </tr>
+      <tr>
+        <table-cell> X 1 </table-cell>
+        <table-cell> BMW </table-cell>
+        <table-cell> BMW </table-cell>
+        <table-cell> BMW </table-cell>
+      </tr>
     </template>
   </table-layout>
 </template>
@@ -21,6 +33,24 @@ export default {
     TableLayout,
     TableHeaderCell,
     TableCell,
+  },
+
+  mounted() {
+    this.displayCars();
+  },
+
+  data() {
+    return {
+      cars: [],
+    };
+  },
+
+  methods: {
+    displayCars() {
+      axios.get("/dashboard").then((response) => {
+        this.cars = response.data;
+      });
+    },
   },
 };
 </script>
