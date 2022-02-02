5cdd40f3abf0c24945618927b78f5686c98112d1
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-02-02 19:55:33 +0800

    added router

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
+
+Route::get('/dashboard', 'App\Http\Controllers\CarController')->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
