505497155b897928374f4348bd4335b339a80754
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-02-02 19:15:41 +0800

    added vue to vite by vite config files

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,31 @@
+import vue from '@vitejs/plugin-vue';
+
 export default ({ command }) => ({
     base: command === 'serve' ? '' : '/build/',
-    outDir: 'public/build',
     publicDir: 'fake_dir_so_nothing_gets_copied',
     build: {
         manifest: true,
+        outDir: 'public/build',
         rollupOptions: {
             input: 'resources/js/app.js',
         },
     },
-    plugins: [{
-        name: 'blade',
-        handleHotUpdate({ file, server }) {
-            if (file.endsWith('.blade.php')) {
-                server.ws.send({
-                    type: 'full-reload',
-                    path: '*',
-                });
-            }
-        },
-    }],
+    // added because its recommended in other project SEO
+    resolve: { alias: { vue: "vue/dist/vue.esm-bundler.js" } },
+    plugins: [
+        // for vue
+        vue(),
+        // for blade
+        {
+            name: 'blade',
+            handleHotUpdate({ file, server }) {
+                if (file.endsWith('.blade.php')) {
+                    server.ws.send({
+                        type: 'full-reload',
+                        path: '*',
+                    });
+                }
+            },
+        }
+    ],
 });
