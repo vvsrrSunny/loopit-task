b006a9f599154865ad05eeca2275bafc430e802e
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-02-02 21:56:43 +0800

    added basic table

--- resources/js/components/CarsTable.vue
+++ resources/js/components/CarsTable.vue
@@ -1,8 +1,52 @@
-<template></template>
+<template>
+  <table-layout>
+    <template #heading>
+      <th
+        scope="col"
+        class="
+          px-6
+          py-3
+          text-left text-xs
+          font-medium
+          text-gray-500
+          uppercase
+          tracking-wider
+        "
+      >
+        Car Name
+      </th>
+      <th
+        scope="col"
+        class="
+          px-6
+          py-3
+          text-left text-xs
+          font-medium
+          text-gray-500
+          uppercase
+          tracking-wider
+        "
+      >
+        Auto make
+      </th>
+    </template>
+    <template #body>
+      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+        X 1
+      </td>
+      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+        BMW
+      </td>
+    </template>
+  </table-layout>
+</template>
 
 <script>
 import TableLayout from "./TableLayout.vue";
+
 export default {
-    TableLayout
+  components: {
+    TableLayout,
+  },
 };
 </script>
