df6533e8c25735adf1a044aaa2e0b2525f7a1f9e
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-02-02 21:31:11 +0800

    removed the unesessory files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,23 @@
 import '../css/app.css';
 
 import './bootstrap';
+
+import { createApp, defineAsyncComponent } from "vue";
+
+
+var app = createApp({
+    data() {
+        return {
+            isSetupSuccess: "grand success",
+        };
+    },
+});
+
+// Registering a global component
+app.component(
+    "DashboardPage",
+    defineAsyncComponent(() =>
+        import ("./components/pages/DashboardPage.vue"))
+);
+
+window.app = app.mount("#app");
---
+++ resources/js/components/pages/DashboardPage.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1> vue is working!! </h1>
+</template>
