bd6f9c1a3f6cdcffa2d23e82e19902933762bcf1
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-02-02 20:24:37 +0800

    added test cases

--- tests/Feature/CarTest.php
+++ tests/Feature/CarTest.php
@@ -1,1 +1,18 @@
 <?php
+
+use App\Models\User;
+
+test('non registered users cannot view the dashboard', function () {
+    $this->get(route('dashboard'))
+        ->assertStatus(302);
+});
+
+test('registered users can view the dashboard', function () {
+    $user = User::factory()->create();
+
+    $this->actingAs($user)->get(route('dashboard'))
+        ->assertStatus(200);
+});
+
+
+
