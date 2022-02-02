985eee88814a8319bbda18d5bc5fd4f71a200b7f
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-02-02 21:43:26 +0800

    dashboard ready

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,33 +1,45 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+</head>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+<body class="font-sans antialiased">
+    <div>
+        <div class="bg-indigo-600 pb-32">
+            <nav class="bg-indigo-600">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="border-b border-indigo-300">
+                        <div class="flex items-center justify-between h-16 px-4 sm:px-0">
+                            <div class="flex items-center">
+                                <div class="hidden md:block">
+                                    <div class="flex items-baseline space-x-4">
+                                        <a href="#" class="bg-indigo-700 text-white px-3 py-2 rounded-md text-sm font-medium">Dashboard</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
-    <body id="app" class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
+            <header class="py-10">
+                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <h1 class="text-3xl font-bold text-white">
+                        Business Data Table
+                    </h1>
+                </div>
             </header>
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
         </div>
-        {{ vite_assets() }}
-    </body>
+
+        <main class="-mt-32">
+            <div class="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
+                <div id="app" class="bg-white rounded-lg shadow px-2 py-6 sm:px-6"> {{$slot}}</div>
+            </div>
+        </main>
+    </div>
+
+    @stack('scripts')
+
+    {{ vite_assets() }}
+</body>
+
 </html>
