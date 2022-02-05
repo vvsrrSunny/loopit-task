9647417df0cb6ff6dcf154bc511ff46ff7703b40
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-02-05 18:48:44 +0800

    added links

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Sunny work') }}</title>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -20,9 +20,12 @@
             @auth
             <a href="{{ url('/dashboard') }}" class="bg-indigo-700 text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer hover:opacity-75">Dashboard</a>
             @else
-            <a href="{{ route('login') }}" class="bg-indigo-700 text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer hover:opacity-75">Log in</a>
 
-            @if (Route::has('register'))
+            @if (! Route::is('login'))
+            <a href="{{ route('login') }}" class="bg-indigo-700 text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer hover:opacity-75">Log in</a>
+            @endif
+
+            @if (Route::has('register') && (! Route::is('register')))
             <a href="{{ route('register') }}" class="bg-indigo-700 text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer hover:opacity-75">Register</a>
             @endif
             @endauth
