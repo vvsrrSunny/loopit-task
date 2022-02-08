b213971141353b5e8a30bbaf4a440eae184c854f
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-02-08 19:38:37 +0800

    moving code to vue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,45 +6,8 @@
 </head>
 
 <body class="font-sans antialiased">
-    <div>
-        <div class="bg-indigo-600 pb-32">
-            <nav class="bg-indigo-600">
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div class="border-b border-indigo-300">
-                        <div class="flex items-center justify-between h-16 px-4 sm:px-0">
-                            <a href="/">
-                                <x-application-logo class="w-10 h-10 fill-current text-red-500 bg-red-200"/>
-                            </a>
-                            <div class="flex items-center">
-                                <div class="hidden md:block">
-                                    <div class="flex items-baseline space-x-4">
-                                        <form method="POST" action="{{ route('logout') }}">
-                                            @csrf
-                                            <input class=" bg-indigo-700 text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer hover:opacity-75" type="submit" value="logout">
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </nav>
 
-            <header class="py-10">
-                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <h1 class="text-3xl font-bold text-white">
-                        All cars available
-                    </h1>
-                </div>
-            </header>
-        </div>
-
-        <main class="-mt-32">
-            <div class="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
-                <div id="app" class="bg-white rounded-lg shadow px-2 py-6 sm:px-6"> {{$slot}}</div>
-            </div>
-        </main>
-    </div>
+    <div id="app" class="bg-white rounded-lg shadow px-2 py-6 sm:px-6"> {{$slot}}</div>
 
     @stack('scripts')
 
