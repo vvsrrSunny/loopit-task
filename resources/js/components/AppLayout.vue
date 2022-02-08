c48106dfa659adf1dfbb6531dcb5cec4ee485fec
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-02-08 20:21:48 +0800

    updated the ui

--- resources/js/components/AppLayout.vue
+++ resources/js/components/AppLayout.vue
@@ -35,13 +35,7 @@
         </div>
       </nav>
 
-      <header class="py-10">
-        <div
-          class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between"
-        >
-          <h1 class="text-3xl font-bold text-white">All cars available</h1>
-        </div>
-      </header>
+      <the-header />
     </div>
 
     <main class="-mt-32">
@@ -54,9 +48,11 @@
 
 <script>
 import ApplicationLogo from "./ApplicationLogo.vue";
+import TheHeader from "./Header.vue";
 export default {
   components: {
     ApplicationLogo,
+    TheHeader,
   },
   methods: {
     logout() {
---
+++ resources/js/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+          <header class="py-10">
+        <div
+          class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between"
+        >
+          <h1 class="text-3xl font-bold text-white">All cars available</h1>
+        </div>
+      </header>
+</template>
