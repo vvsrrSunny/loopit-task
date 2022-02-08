39bfe52defd0ca3ff27c68aa505aa72c8c236664
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-02-08 20:41:45 +0800

    added button

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,9 +1,22 @@
 <template>
-          <header class="py-10">
-        <div
-          class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between"
-        >
-          <h1 class="text-3xl font-bold text-white">All cars available</h1>
-        </div>
-      </header>
+  <header class="py-10">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between">
+      <h1 class="text-3xl font-bold text-white">All cars available</h1>
+
+      <a
+        class="
+          bg-yellow-600
+          text-white
+          px-3
+          py-2
+          rounded-md
+          text-md
+          font-semibold
+          cursor-pointer
+          hover:opacity-75
+        "
+        >Add Car</a
+      >
+    </div>
+  </header>
 </template>
